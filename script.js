455ab46a070eecd1cb3d3da5fc3b326b45fb0ffd
unknown <catcorbin0919@gmail.com>
2023-11-30 22:55:21 -0700

    creating schemas

--- script.js
+++ script.js
@@ -12,3 +12,83 @@ mongoose.connect('mongodb://localhost/social-network', {
   useCreateIndex: true,
   useFindAndModify: false,
 });
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+  });
+  
+  
+  const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  });
+  
+  
+  const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^\S+@\S+\.\S+$/,
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  });
+  
+  
+  // Virtuals and Schema Settings
+  userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+  });
+  
+  
+  thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
+  
